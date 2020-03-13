030d9b4efa67dcd3fbf1082132721556d2a79f98
AJIT-SINGH-RAWAT <47390463+AJIT-SINGH-RAWAT@users.noreply.github.com>
2020-03-13 09:20:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
 
+document.querySelector('.getjokes').addEventListener('click',loaddata);
+
+function loaddata(e){
+ 
+    const number=document.getElementById('number').value;
+    
+    const xhr=new XMLHttpRequest();
+
+    xhr.open('GET',`http://api.icndb.com/jokes/random/${number}`,true);
+
+    xhr.onload=function(){
+       const response=JSON.parse(this.responseText);
+       
+       let output='';
+       if (response.type==='success'){
+           response.value.forEach(function(i){
+               output+=`<li>${i.joke}</li>`;
+           })
+       }
+       else{
+           output+=`<li>Something Went Wrong</li>`;
+       }
+       document.querySelector('.jokes').innerHTML=output;
+    }
+    
+
+    xhr.send();
+   e.preventDefault();
+}
